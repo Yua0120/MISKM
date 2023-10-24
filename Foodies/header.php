652ac4d4ede302ebcad3f26b445058956bba6270
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2023-10-24 12:44:50 +0900

    ヘッダー部分のphpくんでみた

---
+++ Foodies/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="header.css">
+    <title></title>
+</head>
+<body>
+    <header class="header">
+        <!-- ヘッダーロゴ -->
+        <div class="logo">Foodies</div>
+    </header>
+</body>
+
