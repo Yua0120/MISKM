152010ae443bc95241d95d2b73268ea124222e27
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2023-11-09 12:38:15 +0900

    info_updateの処理実装

---
+++ MISKM/Foodies/php/info_update-output.php
@@ -0,0 +1,38 @@
+<?php session_start()?>
+<?php require 'header.php';?>
+<?php require 'connect.php'; ?>
+<?php 
+    $pdo = new PDO($connect,USER,PASS);
+    if(isset($_SESSION['User'])){
+        $id=$_SESSION['User']['id'];
+        $sql=$pdo->prepare('select * from Customer where id != ? and nickname = ?');
+        $sql->execute([$id, $_POST['Nickname']]);
+    }else{
+        $sql=$pdo->prepare('select * from Customer where nickname = ?');
+        $sql->execute([$_POST['nickname']]);
+    }
+    if(empty($sql->fetchAll())){
+        if (isset($_SESSION['User'])) {
+            $sql=$pdo->prepare('update User set name = ?, nickname= ?, address = ?, tel_number = ?, zip_code = ? where id = ?');
+            $sql->execute([
+                $_POST['name'],$_POST['nickname'],$_POST['address'],$_POST['phonenumber'],$_POST['postcode'],$id                
+            ]);
+            $_SESSION['User'] = [
+                'id' => $id, 'name' => $_POST['name'],
+                'nickname' => $_POST['nickname'], 'address' => $_POST['address'],
+                'tel_number'=> $_POST['phonenumber'],'zip_code' => $_POST['postcode'],
+            ];
+            header("Location: ../Top.html");
+            exit;
+        }else{
+            $sql=$pdo->prepare('insert into User values(null,?,?,?,?,?)');
+            $sql->execute([
+                $_POST['name'],$_POST['nickname'],$_POST['address'], $_POST['phonenumber'],$_POST['postcode']
+            ]);
+            header("Location: ../Top.html");
+            exit;
+        }
+    }else{
+        echo 'ログイン名が既に使用されていますので、変更してください。';
+    }    
+?>
