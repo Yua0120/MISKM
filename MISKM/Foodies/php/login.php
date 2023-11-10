5cfcf5ba9c85851ea274b27b177fe6aacfd615d5
Aoi1591 <2201039@s.asojuku.ac.jp>
2023-11-10 11:57:38 +0900

    ｃｓｓの接続

--- MISKM/Foodies/php/login.php
+++ MISKM/Foodies/php/login.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/centerYoo.css">
-    <link rel="stylesheet" href="./css/template.css">
-    <link rel="stylesheet" href="./css/login.css">
+    <link rel="stylesheet" href="../css/centerYoo.css">
+    <link rel="stylesheet" href="../css/template.css">
+    <link rel="stylesheet" href="../css/login.css">
     <title>login.html</title>
 </head>
 <body>
