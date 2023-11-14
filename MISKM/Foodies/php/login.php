d094fbf487b42abafb6aacb1bce83d10fa4b6b9e
Aoi1591 <2201039@s.asojuku.ac.jp>
2023-11-14 11:57:39 +0900

    Create login.php

---
+++ MISKM/Foodies/php/login.php
@@ -0,0 +1,66 @@
+<?php require 'header.php'?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<<<<<<< HEAD
+    <link rel="stylesheet" href="../css/centerYoo.css">
+    <link rel="stylesheet" href="../css/template.css">
+    <link rel="stylesheet" href="../css/login.css">
+=======
+    <link rel="stylesheet" href="./css/centerYoo.css">
+    <link rel="stylesheet" href="./css/template.css">
+    <link rel="stylesheet" href="./css/login.css">
+>>>>>>> main
+    <title>login.html</title>
+</head>
+<body>
+
+    <h1>Choice is yours</h1>
+
+<<<<<<< HEAD
+    <form action="login-output.php" method="post">
+=======
+<<<<<<<< HEAD:MISKM/Foodies/login.php
+    <form action="login-output.php" method="post">
+========
+    <form action="Top.php" method="post">
+>>>>>>>> main:MISKM/Foodies/php/login.php
+>>>>>>> main
+
+        <div class="container">
+        <div class="left-aligned-text">
+        <div class="login-input">
+            <p>NickName
+                <input type="text" name="nickname" style="width: 200px; height: 30px;"></p>
+            <p>PassWord
+                <input type="password" name="password" style="width: 200px; height: 30px;"></p>
+        </div>
+        <div class="login-confirmation">
+<<<<<<< HEAD
+            <a href="newpass.html">パスワードを忘れた方はこちら</a><br>
+        </div>
+        <div class="NEWlogin-confirmation">
+            <a href="U_reg.html">新規の方はこちら</a><br>
+=======
+            <a href="U_check.php">パスワードを忘れた方はこちら</a><br>
+        </div>
+        <div class="NEWlogin-confirmation">
+            <a href="U_reg.php">新規の方はこちら</a><br>
+>>>>>>> main
+        </div>
+        </div>
+        </div>
+
+        <div class="login-button">
+<<<<<<< HEAD
+            <p><button class="example" type="submit" name="login"  onclick="location.href='Top.html'"><span>Login</span></button></p>
+=======
+            <p><button class="example" type="submit" name="login"><span>Login</span></button></p>
+>>>>>>> main
+        </div>
+    </form>
+    <center><h2>New Member registration</h2></center>
+
+</body>
