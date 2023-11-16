6613ef5a207eae033c72f12c320bba99b9f73da1
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2023-11-16 12:37:27 +0900

    newpass編集

--- MISKM/Foodies/php/newpass.php
+++ MISKM/Foodies/php/newpass.php
@@ -1,38 +1,45 @@
-
 <?php require 'header.php'?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
     <link rel="stylesheet" href="./css/centerYoo.css">
     <link rel="stylesheet" href="./css/template.css">
     <link rel="stylesheet" href="./css/newpass.css">
     <title>新しいパスワード</title>
 </head>
+<body>
 <?php require 'FoodiesTitle.php' ?>
+<?php
+    echo '<div id="app">';
+    echo '<form action="newpass-output.php">';        
+    echo '<br><br><br>';
+    echo '<div class="container">';
+    echo '<div class="left-aligned-text">';
+    echo '<div class="login-input">';
 
-<div id="app">            
-    <br><br><br>
-        <div class="container">
-        <div class="left-aligned-text">
-        <div class="login-input">
+        echo 'New PassWord';
+        echo '<input type="password"  class="in" name="new_pass" id="new_password" v-model="pass" placeholder="8文字以上16文字以下で入力してください"><br>';
+        echo '<p v-if="isInValidPass" class="error">';
+            echo '8文字以上16文字以下で入力してください。</p>';
 
-            New PassWord
-            <input type="password"  class="in" name="new_pass" id="new_password" v-model="pass" placeholder="8文字以上16文字以下で入力してください"><br>
-            <p v-if="isInValidPass" class="error">
-                8文字以上16文字以下で入力してください。</p>
+        echo '<p><input type="password" class="in" name="new_pass2" id="new_password" v-model="pass2" placeholder="もう一度パスワードを入力してください"></p>';
+        echo '<p v-if="isInValidPassW" class="error">';
+            echo 'パスワードが一致していません。確認してください。</p>';
 
-         <p><input type="password" class="in" name="new_pass2" id="new_password" v-model="pass2" placeholder="もう一度パスワードを入力してください"></p>
-            <p v-if="isInValidPassW" class="error">
-                パスワードが一致していません。確認してください。</p>
+        echo '<div class="login-button">';
+            echo '<p><button class="example" type="submit"><span>Login</span></button></p>';
+        echo '</div>';
 
-        <div class="login-button">
-            <p><button class="example" type="submit" name="login"><span>Login</span></button></p>
-        </div>
-
-        </div>
-        </div>
-        </div>    
-</div>
-
+    echo  '</div>';
+    echo  '</div>';
+    echo  '</div>';    
+    echo  '</form>';
+    echo  '</div>';
+?>
+</body>
     <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
         <script src="./script/newpass.js"></script>
-
-</body>
 </html> 
