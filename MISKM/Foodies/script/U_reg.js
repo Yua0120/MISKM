7258b4bda3a17778db7dbd3818f22fdc9559bc0b
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2023-11-21 15:06:56 +0900

    編集

--- MISKM/Foodies/script/U_reg.js
+++ MISKM/Foodies/script/U_reg.js
@@ -1,33 +1,29 @@
-document.addEventListener('DOMContentLoaded', function () {
     new Vue({
         el: '#app',
         data: {
-            email: '',
-            password1: '',
-            password2: '',
-            question: '',
-            isEmailError: false,
-            isLengthError: false,
-            isMatchError: false,
-            isQuestionError: false
+          email: '',
+          password1: '',
+          password2: '',
+          question: '',
+          isEmailError: false,
+          isLengthError: false,
+          isMatchError: false,
+          isQuestionError: false
         },
         methods: {
-            validateForm: function () {
-                this.isEmailError = !this.validateEmail(this.email);
-                this.isLengthError = this.password1.length < 8 || this.password1.length > 16;
-                this.isMatchError = this.password1 !== this.password2;
-                this.isQuestionError = this.question.trim() === '';
+          validateForm: function () {
+            this.isEmailError = !this.validateEmail(this.email);
+            this.isLengthError = this.password1.length < 8 || this.password1.length > 16;
+            this.isMatchError = this.password1 !== this.password2;
+            this.isQuestionError = this.question.trim() === '';
 
-                if (!this.isEmailError && !this.isLengthError && !this.isMatchError && !this.isQuestionError) {
-                    // エラーがない場合、フォームを送信
-                    document.querySelector('form').submit();
-                }
-            },
-            validateEmail: function (email) {
-                // 基本的なメールのバリデーションのための正規表現
-                var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-                return emailRegex.test(email);
-            }
+            // 各フィールドがエラーでないかどうかを返す
+            return !this.isEmailError && !this.isLengthError && !this.isMatchError && !this.isQuestionError;
+          },
+          validateEmail: function (email) {
+            // 基本的なメールのバリデーションのための正規表現
+            var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            return emailRegex.test(email);
+          }
         }
-    });
-});
+      });
