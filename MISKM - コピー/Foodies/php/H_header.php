4e90918da7ca2a383c8dfa3a819f3753d35a7dc9
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2023-11-21 11:25:28 +0900

    a

---
+++ MISKM - コピー/Foodies/php/H_header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/header.css">
+    <title></title>
+</head>
+<body>
+    <header class="header">
+        <!-- ヘッダーロゴ -->
+        <div class="logo">Foodies</div>
+        <!-- ハンバーガーメニュー部分 -->
+        <div class="nav">
+        <!-- ハンバーガーメニューの表示・非表示を切り替えるチェックボックス -->
+        <input id="drawer_input" class="drawer_hidden" type="checkbox">
+        <!-- ハンバーガーアイコン -->
+        <label for="drawer_input" class="drawer_open"><span></span></label>
+    
+        <!-- メニュー -->
+        <nav class="nav_content">
+            <ul class="nav_list">
+            <li class="nav_item"><a href="">トップ</a></li>
+            <li class="nav_item"><a href="">投稿</a></li>
+            <li class="nav_item"><a href="">カート</a></li>
+            <li class="nav_item"><a href="">注文履歴</a></li>
+            <li class="nav_item"><a href="">アカウント情報</a></li>
+            </ul>
+        </nav>
+
+        </div>
+    </header>
+</body>
