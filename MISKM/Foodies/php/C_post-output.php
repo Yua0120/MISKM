053492cc12279a80fee9caa8bca6bc1b5f23103f
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2023-11-21 13:46:04 +0900

    Update C_post-output.php

--- MISKM/Foodies/php/C_post-output.php
+++ MISKM/Foodies/php/C_post-output.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    if(isset($_SESSION['User'])){
+    if(!isset($_SESSION['User'])){
         header("Location: ./C_post-input.php?flag=none");
         exit;
     }else{
