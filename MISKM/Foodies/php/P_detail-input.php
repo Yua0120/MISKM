d839d8a6026698494ba2e0a7d82777619d40530e
m1kqn <137014023+suzuki689@users.noreply.github.com>
2023-11-24 14:46:09 +0900

    Update P_detail-input.php

--- MISKM/Foodies/php/P_detail-input.php
+++ MISKM/Foodies/php/P_detail-input.php
@@ -8,32 +8,6 @@
 <title>商品詳細</title>
 </header>
 <?php require 'Foodiesall.php' ?>
-<body>
-    <header class="header">
-        <!-- ヘッダーロゴ -->
-        <div class="logo">Foodies</div>
-        <!-- ハンバーガーメニュー部分 -->
-        <div class="nav">
-        <!-- ハンバーガーメニューの表示・非表示を切り替えるチェックボックス -->
-        <input id="drawer_input" class="drawer_hidden" type="checkbox">
-        <!-- ハンバーガーアイコン -->
-        <label for="drawer_input" class="drawer_open"><span></span></label>
-    
-        <!-- メニュー -->
-        <nav class="nav_content">
-            <ul class="nav_list">
-            <li class="nav_item"><a href="Top.php">トップ</a></li>
-            <li class="nav_item"><a href="">投稿</a></li>
-            <li class="nav_item"><a href="Cart">カート</a></li>
-            <li class="nav_item"><a href="">注文履歴</a></li>
-            <li class="nav_item"><a href="">アカウント情報</a></li>
-            </ul>
-        </nav>
-
-        </div>
-    </header>
-
-    
     <div class="shohin-detail">
         <div class="carousel">
             <!-- スライドのリスト -->
