913f6a0f2329552e5f2bfa3fe976c520484bcfc8
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2023-11-24 10:53:04 +0900

    Update C_post-input.php

--- MISKM/Foodies/php/C_post-input.php
+++ MISKM/Foodies/php/C_post-input.php
@@ -1,7 +1,7 @@
 <?php require 'header.php' ?>
 <link rel="stylesheet" href="../css/coordinate.css">
 <link rel="stylesheet" href="../css/template.css">
-<link rel="stylesheet" href="../css/coordinate.css">
+<link rel="stylesheet" href="../css/coordinate.css?v=1.0.0">
 <title>CoordinatePost.html</title>
 </head>
 <?php require 'FoodiesMenu.php' ?>
