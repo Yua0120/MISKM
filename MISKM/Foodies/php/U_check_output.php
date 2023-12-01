551bdfaf9294fc17f0ce471cabe4ace9c3838905
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2023-12-01 14:17:54 +0900

    あおいの編集したお

--- MISKM/Foodies/php/U_check_output.php
+++ MISKM/Foodies/php/U_check_output.php
@@ -1,26 +1,43 @@
 <?php session_start();?>
 <?php require 'connect.php';?>
 <?php
-/*unset($_SESSION['User']);*/
-$pdo=new PDO($connect,USER,PASS);
-$sql=$pdo->prepare('select * from User where mail=?');
-$result = $sql->execute([$_POST['mail']]);
-foreach ($sql as $row){
-    if(strcmp($_POST['mail'],$row['mail']) == 0 && strcmp($_POST['question'],$row['question']) == 0){
-    $_SESSION['User']=[
-        'id' =>$row['id'],'mail'=>$row['mail'],
-        'name'=>$row['name'],'zip_code' => $row['zip_code'],
-        'addres'=>$row['addres'],'tel_number'=>$row['tel_number'],
-        'question'=>$row['question']];
+unset($_SESSION['User']);
+
+if (isset($_POST['mail']) && isset($_POST['question'])) {
+    try {
+        $pdo = new PDO($connect, USER, PASS);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $sql = $pdo->prepare('SELECT * FROM User WHERE mail = ?');
+        $result = $sql->execute([$_POST['mail']]);
+
+        if ($result) {
+            $row = $sql->fetch(PDO::FETCH_ASSOC);
+
+            if ($row && strcmp($_POST['question'], $row['question']) == 0) {
+                $_SESSION['User'] = [
+                    'id' => $row['id'],
+                    'mail' => $row['mail'],
+                    'name' => $row['name'],
+                    'zip_code' => $row['zip_code'],
+                    'addres' => $row['addres'],
+                    'tel_number' => $row['tel_number'],
+                    'question' => $row['question']
+                ];
+
+                header("Location:./newpass.php");
+                exit;
+            } else {
+                header("Location:./U_check_input.php?flag=fail");
+                exit;
+            }
+        } else {
+            echo "SQL エラー: " . $sql->errorInfo()[2];
+            // エラーログや適切なエラーハンドリングを追加することが望ましい
+        }
+    } catch (PDOException $e) {
+        echo "エラー: " . $e->getMessage();
+        // エラーログや適切なエラーハンドリングを追加することが望ましい
     }
-    
-}
-if(isset($_SESSION['User'])){
-    header("Location:./newpass.php");
-    exit;
-}else if(!($result)){
-    header("Location:./U_check_input.php?flag=fail");
-    exit;
 }
 ?>
-<!--<button type="submit">Next</button>-->
