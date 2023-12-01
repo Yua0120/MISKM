ff447907f8534fb26114258ddc4866594a80abab
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2023-12-01 10:10:40 +0900

    Update C_post-output.php

--- MISKM/Foodies/php/C_post-output.php
+++ MISKM/Foodies/php/C_post-output.php
@@ -6,7 +6,7 @@
     }else{
         require 'connect.php';
         $pdo = new PDO($connect, USER, PASS);
-        $product_id = $_POST['product_name']+$_POST['product_size'];
+        $product_id = $_POST['product_name'] . '-' . $_POST['product_size'];
         $sql = $pdo->prepare('insert into Post(user_id,product_id,product_size,image_path,comment) values (?,?,?,?,?)');
         $success = $sql->execute([$_SESSION['User']['id'], $product_id, $_POST['image_path'], $_POST['comment']]);
         if ($success) {
