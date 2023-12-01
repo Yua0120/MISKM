a652724cd4e812560b31f029a87f92667f507a3f
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2023-12-01 14:04:17 +0900

    競合解消中

--- MISKM/Foodies/php/U_info.php
+++ MISKM/Foodies/php/U_info.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/template.css">
     <link rel="stylesheet" href="css/touroku.css">
     <title>ユーザー個人情報登録</title>
 </head>
