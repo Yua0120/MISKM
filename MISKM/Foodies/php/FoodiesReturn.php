ccc4813d5f0113459501afae2edf044303ae3ccf
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2023-12-01 10:19:53 +0900

    change

--- MISKM/Foodies/php/FoodiesReturn.php
+++ MISKM/Foodies/php/FoodiesReturn.php
@@ -1,12 +1,10 @@
-<link rel="stylesheet" href="../css/header.css">
 <body>
     <header class="header">
     <?php
         // これがボタン
         if (!empty($r) && (strpos($r, $h) !== false)) :
         ?>
-        <div class="post-detail-button is_prev">
-            <a onclick="window.location.href='<?= $r ?>'" class="return-button">＜ return</a></div>        
+        <div class="post-detail-button is_prev"><a id="return-button" href="<?= $r ?>">< return</a></div>
         <?php endif ?>
         <!-- ヘッダーロゴ -->
         <div class="logo">Foodies</div>
