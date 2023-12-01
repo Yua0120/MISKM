5da9cf080803189810c00113619a0320d37061e7
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2023-12-01 15:06:30 +0900

    編集

--- MISKM/Foodies/php/U_check_input.php
+++ MISKM/Foodies/php/U_check_input.php
@@ -3,29 +3,31 @@
 <link rel="stylesheet" href="../css/U_check.css">
 <title>本人確認画面</title>
 </head>
-<body> <!-- この開始のbodyタグを追加 -->
-<?php require 'FoodiesTitle.php'; ?>
+<body>
+<?php require 'FoodiesReturn.php'; ?>
+
+
+    <form action="U_check_output.php" method="post">
+    <div class="container">
+    <div class="left-aligned-text">
+    <div class="login-input">
+
+        メール<br>
+        <input type="email" class="in" name ="mail" id="mail" required><br>
+
+        秘密の質問<br>
+        <input type="text" class="in" name="question" id="question" placeholder="卒業した小学校名を入力してください" required><br>
+
+        <div class="next-button">
+            <p><button class="example" type="submit"><span>Next</span></button></p>
+        </div>
+
+    </div>
+    </div>
+    </div>
+    </form>
+
 <?php
-    echo '<form action="U_check_output.php" method="post">';
-    echo '<div class="container">';
-    echo '<div class="left-aligned-text">';
-    echo '<div class="login-input">';
-
-    echo 'メール<br>';
-    echo '<input type="email" class="in" name ="mail" id="mail" required><br>';
-
-    echo '秘密の質問<br>';
-    echo '<input type="text" class="in" name="question" id="question" placeholder="卒業した小学校名を入力してください" required><br>';
-
-    echo '<div class="next-button">';
-    echo '<p><button class="example" type="submit"><span>Next</span></button></p>';
-    echo '</div>';
-
-    echo '</div>';
-    echo '</div>';
-    echo '</div>';
-    echo '</form>';
-
     // $_GET['flag']がセットされているか確認(本人確認に失敗した場合)
     if (isset($_GET['flag']) && $_GET['flag'] == 'fail') {
         echo '<p class="error">本人確認に失敗しました。もう一度入力してください。</p>';
