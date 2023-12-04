023abce80458414e6bf52b95597d3e64e5051d36
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2023-12-04 11:15:51 +0900

    hamburgerの作成と、編集

---
+++ MISKM/Foodies/php/hamburger.php
@@ -0,0 +1,34 @@
+<link rel="stylesheet" href="../css/hamburger.css">
+<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+    <title>ハンバーガーメニュー専用</title>
+</head>
+<!--ヘッダー↓-->
+<header>
+  <!-- spハンバーガーメニュー ↓ -->
+  <div class="sp_nav">
+    <div class="overlay" id="js_overlay"></div>
+    <div class="hamburger" id="js_hamburger">
+      <span class="hamburger_linetop"></span>
+      <span class="hamburger_linecenter"></span>
+      <span class="hamburger_linebottom"></span>
+    </div>
+    <div class="sidemenu">
+      <nav>
+        <ul>
+          <li><a href="Top.php">トップ</a></li>
+          <li><a href="">投稿</a></li>
+          <li><a href="">投稿一覧</a></li>
+          <li><a href="">カート</a></li>
+          <li><a href="">注文履歴</a></li>
+	      <li><a href="">マイページ</a></li>
+	      <li><a href="">アカウント情報更新</a></li>
+	      <li><a href="">ログアウト</a></li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+  <!-- spハンバーガーメニュー ↑ -->
+</header>
+<!--ヘッダー↑-->
+
+<!-- ハンバーガーメニューを表示させたいときは、/bodyから下を消してhamburger.phpを呼ぶんだ。 -->
