9655afe5391e2ba8f54bf954dd4e688a370487c7
m1kqn <137014023+suzuki689@users.noreply.github.com>
2023-12-04 12:18:52 +0900

    P_detail

--- MISKM/Foodies/php/P_detail-input.php
+++ MISKM/Foodies/php/P_detail-input.php
@@ -10,16 +10,13 @@
 $pdo = new PDO($connect, USER, PASS);
 
 // ログインしている場合、セッションから user_id を取得
-$user_id = isset($_SESSION['User_id']) ? $_SESSION['User_id'] : '';
+$user_id = isset($_SESSION['User']['id']) ? $_SESSION['User']['id'] : '';
 
 $sql = $pdo->prepare('select * from Product where id=?');
 $sql->execute([$_GET['id'] . '-L']);
-// セッションに登録しておいた商品id情報を破棄
-if (isset($_SESSION['id'])) {
-    unset($_SESSION['id']);
-}
+
 // セッションに選択した商品idを登録しなおす
-$_SESSION['id'] = $_GET['id'];
+//$_SESSION['product_id'] = $_GET['id'];
 
 foreach ($sql as $row) {
 
