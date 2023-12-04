a92cad94d45bc4bd974b670a1e1a5a3bddbdf842
Aoi1591 <2201039@s.asojuku.ac.jp>
2023-12-04 10:34:59 +0900

    注文履歴
    注文履歴CSS作成
    画面に写真の追加
    詳細画面への接続

--- MISKM/Foodies/php/O_his.php
+++ MISKM/Foodies/php/O_his.php
@@ -2,7 +2,7 @@
 <?php require 'connect.php';?>
     <title>注文履歴</title>
 </head>
-<body>
+<?php require 'FoodiesMenu.php';?>
     <p>注文履歴</p>
     <!--スクロールの設定　お試し-->
     <div style="width: 100%; height: 100px; overflow-y: scroll; border: 1px #999999 soild;">
@@ -10,19 +10,23 @@
     /* データベース接続 */
     if (!isset($_SESSION['User'])) {
         $pdo = new PDO($connect, USER, PASS);
-        $sql = "SELECT History.date,Product.name
+        $sql = "SELECT History.date,Product.name,Product.image,History_detail.product_id
                 FROM History
                 JOIN History_detail ON History.id = History_detail.history_id
                 JOIN Product ON History_detail.product_id = Product.id";
         $stmt = $pdo->query($sql);
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        $product_id => $row['product_id'];
         /* 商品一覧 */
           /* echo '<div class="ALL">';*/
         foreach ($result as $row) {
             echo '<div class="main">';
+            echo '<figure class="image">';
+            echo '<img src="/MISKM/img/', $row['image'], '" class="his_img">';
+            echo '</figure>';
             echo '<div class="item">';
             echo "<p>{$row['date']}
-                  {$row['name']}</p>";
+                  <a herf='P_detail-input.php' ?id='".$product_id."'>{$row['name']}</a></p>";
             echo "<br>";
             echo '<p id="line">-</p>';
             echo '</div>'; // .item divを閉じる
