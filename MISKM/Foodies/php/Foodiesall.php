e14f260a9506194a17b49bdf58270cc860a7ea5d
Yua0120 <141288031+Yua0120@users.noreply.github.com>
2023-12-05 13:37:02 +0900

    hamburgerの変更

--- MISKM/Foodies/php/Foodiesall.php
+++ MISKM/Foodies/php/Foodiesall.php
@@ -10,23 +10,27 @@
         <!-- ヘッダーロゴ -->
         <div class="logo">Foodies</div>
 
-        <!-- ハンバーガーメニュー部分 -->
-        <div class="nav">
-        <!-- ハンバーガーメニューの表示・非表示を切り替えるチェックボックス -->
-        <input id="drawer_input" class="drawer_hidden" type="checkbox">
-        <!-- ハンバーガーアイコン -->
-        <label for="drawer_input" class="drawer_open"><span></span></label>
-    
-        <!-- メニュー -->
-        <nav class="nav_content">
-            <ul class="nav_list">
-            <li class="nav_item"><a href="Top.php">トップ</a></li>
-            <li class="nav_item"><a href="C_browsing.php">投稿一覧</a></li>
-            <li class="nav_item"><a href="">カート</a></li>
-            <li class="nav_item"><a href="">注文履歴</a></li>
-            <li class="nav_item"><a href="mypage.php">マイページ</a></li>
-            </ul>
-        </nav>
-
+        <!-- spハンバーガーメニュー ↓ -->
+        <div class="sp_nav">
+            <div class="overlay" id="js_overlay"></div>
+            <div class="hamburger" id="js_hamburger">
+            <span class="hamburger_linetop"></span>
+            <span class="hamburger_linecenter"></span>
+            <span class="hamburger_linebottom"></span>
+            </div>
+            <div class="sidemenu">
+            <nav>
+                <ul>
+                <li><a href="Top.php">トップ</a></li>
+                <li><a href="">投稿</a></li>
+                <li><a href="">投稿一覧</a></li>
+                <li><a href="">カート</a></li>
+                <li><a href="">注文履歴</a></li>
+                <li><a href="">マイページ</a></li>
+                <li><a href="">アカウント情報更新</a></li>
+                <li><a href="">ログアウト</a></li>
+                </ul>
+            </nav>
+            </div>
         </div>
     </header>
