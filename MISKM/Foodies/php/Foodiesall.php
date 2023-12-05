ae24ef56595f7f19b70a790e2e2f7c6bc8f10a09
m1kqn <137014023+suzuki689@users.noreply.github.com>
2023-12-05 09:45:54 +0900

    FoodiesAll

--- MISKM/Foodies/php/Foodiesall.php
+++ MISKM/Foodies/php/Foodiesall.php
@@ -1,12 +1,7 @@
 <body>
 <link rel="stylesheet" href="../css/header.css">
     <header class="header">
-    <?php
-        // これがボタン
-        if (!empty($r) && (strpos($r, $h) !== false)) :
-        ?>
-        <div class="post-detail-button is_prev"><a href="<?= $r ?>" class="return-button">< return</a></div>
-        <?php endif ?>
+        <div class="post-detail-button is_prev"><a id="return-button" href="javascript:history.back();">＜ return</a></div>
         <!-- ヘッダーロゴ -->
         <div class="logo">Foodies</div>
 
