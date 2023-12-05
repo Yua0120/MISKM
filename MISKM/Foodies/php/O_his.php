9f9ddd9659be67617b9ecc3ffd4702d9324605c7
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2023-12-05 14:15:01 +0900

    ハンバーガーメニューの編集

--- MISKM/Foodies/php/O_his.php
+++ MISKM/Foodies/php/O_his.php
@@ -1,6 +1,7 @@
 <?php require 'header.php';?>
 <?php require 'connect.php';?>
 <link rel="stylesheet" href="../css/O_his.css">
+<link rel="stylesheet" href="../css/hamburger.css">
 <title>注文履歴</title>
 </head>
 <?php require 'FoodiesMenu.php';?>
@@ -33,5 +34,4 @@ if (!isset($_SESSION['User'])) {
         echo '<button type="button" onclick="location.href=\'O_pro.php\'">購入手続きへ</button>';
 ?>
 </div>
-</body>
-</html>
+<?php require 'footer.php' ;?>
