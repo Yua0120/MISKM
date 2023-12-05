a75365b6c49219399877dc9e41f27b91de449cca
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2023-12-05 14:54:19 +0900

    編集

--- MISKM/Foodies/php/FoodiesReturn-C_browsing.php
+++ MISKM/Foodies/php/FoodiesReturn-C_browsing.php
@@ -1,6 +1,6 @@
 <body>
         <header class="header">
-        <div class="post-detail-button is_prev"><a id="return-button" href="C_browsing.php">＜ return</a></div>
+        <div class="post-detail-button is_prev"><a id="return-button" href="C_browsing.php">< return</a></div>
         <!-- ヘッダーロゴ -->
         <div class="logo">Foodies</div>
       
