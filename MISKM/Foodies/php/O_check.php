c855142b360701ed1a1f5a5a475e5379b6d65b26
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2023-12-06 12:55:10 +0900

    編集

--- MISKM/Foodies/php/O_check.php
+++ MISKM/Foodies/php/O_check.php
@@ -1,11 +1,13 @@
 <?php require 'header.php';?>
 <?php require 'connect.php';?>
+    <link rel="stylesheet" href="../css/centerYoo.css">
     <link rel="stylesheet" href="../css/O_check.css">
     <title>注文確認</title>
 </head>
 <?php require 'FoodiesReturn.php'; ?>
     <form action="O_con.php" mrthods="post">
-    <div class="main">
+    <div class="container">
+        <div class="left-aligned-text">
     <?php
        echo $_POST['name'],'<br>';/*-名前*/
        echo $_POST['zip_code'],'<br>';/*-郵便番号*/
@@ -17,6 +19,7 @@
        ?>
     <p class="check"> 情報に誤りがないかご確認ください</p>
     <button type="submit" class="example"><span>注文確定</span></button>
+        </div>
     </div>
     </form>
 </body>
