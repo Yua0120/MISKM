e3976f8a906fd35b9097e06360cfafcd945f340b
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2023-12-06 10:15:49 +0900

    ヘッダーの編集

--- MISKM/Foodies/php/C_browsing.php
+++ MISKM/Foodies/php/C_browsing.php
@@ -1,10 +1,12 @@
 <?php session_start(); ?>
-<?php require 'header.php' ?>
 <?php require 'connect.php' ?>
-
-<link rel="stylesheet" href="../css/header.css">
-<link rel="stylesheet" href="../css/C_browsing.css">
-<link rel="stylesheet" href="../css/hamburger.css">
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/C_browsing.css">
+        <link rel="stylesheet" href="../css/hamburger.css">
 
 <title>投稿一覧</title>
 </header>
