4b12361e1237d735a5e25386f2250f5d7692e42c
Aoi1591 <2201039@s.asojuku.ac.jp>
2023-12-06 12:06:57 +0900

    DBの書き方変更

--- MISKM/Foodies/php/Cart_delete.php
+++ MISKM/Foodies/php/Cart_delete.php
@@ -1,10 +1,34 @@
 <?php session_start(); ?>
 <?php require 'connect.php';?>
 <?php
-    $pdo = new PDO($connect, USER, PASS);
-    $sql = "ALTER TABLE Cart
-    DROP product_id;";/*カート内データ削除*/
-  
+    /*$pdo = new PDO($connect, USER, PASS);
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($result as $row) {
+    $sql = "DELETE FROM Cart
+           WHERE product_id = '$row['product_id']';
+           ";/*カート内データ削除*/
+   /* }
      header("Location:./Cart.php");
      exit;
+
+     <?php*/
+
+     $pdo = new PDO($connect, USER, PASS);
+
+     if (isset($_SESSION['User'])) {
+     $user_id = $_SESSION['User']['id'];
+
+     if (isset($_POST['product_id'])) {
+        $product_id = $_POST['product_id'];
+
+        $deleteSql = "DELETE FROM Cart WHERE user_id = ? AND product_id = ?";
+        $deleteStmt = $pdo->prepare($deleteSql);
+        $deleteStmt->execute([$user_id, $product_id]);
+
+        header("Location: ./Cart.php");
+        exit;
+       } 
+     }
+?>
+
   ?>
