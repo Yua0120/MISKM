ddd81b78ec6931725494124e9689f74be7cc454c
m1kqn <137014023+suzuki689@users.noreply.github.com>
2023-12-12 17:32:00 +0900

    Update FoodiesLogout.php

--- MISKM/Foodies/php/FoodiesLogout.php
+++ MISKM/Foodies/php/FoodiesLogout.php
@@ -1,6 +1,6 @@
 <body>
     <header class="header">
-        <div class="post-detail-button is_prev"><a id="return-button" href="logout-check.php">< return</a></div>
+        <div class="post-detail-button is_prev"><a id="return-button" href="logout-check.php">< logout</a></div>
         
         <!-- ヘッダーロゴ -->
         <div class="logo">Foodies</div>
