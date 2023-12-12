cde880ef0069cda76ba271a75a6d45ca2b72d42f
ayyn0863 <140891515+ayyn0863@users.noreply.github.com>
2023-12-12 12:25:04 +0900

    編集

--- MISKM/Foodies/php/info_update-output.php
+++ MISKM/Foodies/php/info_update-output.php
@@ -1,3 +1,4 @@
+
 <?php session_start()?>
 <?php require 'connect.php'; ?>
 <?php 
@@ -7,21 +8,34 @@
     try {
         $sql = $pdo->prepare('select * from User where id != ? and nickname = ?');
         $sql->execute([$id, $_POST['nickname']]);
-        //アップロードされた画像の一時的な保管場所のアドレス(テンポラリファイル)のパス'tmp_file'を取得
+        
+        // アップロードされた画像の一時的な保管場所のアドレス(テンポラリファイル)のパス'tmp_file'を取得
         $tmp_path = $_FILES['image_path']['tmp_name'];
+        
         // 画像の保存先フォルダと保存先のファイル名
-        $img_folder = $img_folder =  '../../img/icon_img/';//画像の保存先フォルダ
-        $img_filename = uniqid() . '_' . basename($_FILES['image_path']['name']);//アップロードされた画像の名前を一意的なものに変更
-        $img_path = $img_folder . $img_filename;//アップロードされた画像の最終的なパスを取得
-        move_uploaded_file($tmp_path, $img_path);//画像をicon_imgに保存する
+        $img_folder = '../../img/icon_img/'; // 画像の保存先フォルダ
+        $img_filename = uniqid() . '_' . basename($_FILES['image_path']['name']); // アップロードされた画像の名前を一意的なものに変更
+        $img_path = $img_folder . $img_filename; // アップロードされた画像の最終的なパスを取得
+        move_uploaded_file($tmp_path, $img_path); // 画像をicon_imgに保存する
+        
         // 下のifは同じニックネームがあるかないかのチェック
         if ($sql->rowCount() == 0) {
             if ($id) {
-                //　ここは登録してあるユーザーデータを更新する処理
-                $sql = $pdo->prepare('update User set name = ?, nickname = ?, addres = ?, tel_number = ?, zip_code = ? ,flag = 1, icon_image_path = ? where id = ?');
+                // ここは登録してあるユーザーデータを更新する処理
+                $sql = $pdo->prepare('update User set 
+                    name = COALESCE(?, name), 
+                    nickname = COALESCE(?, nickname), 
+                    addres = COALESCE(?, addres), 
+                    tel_number = COALESCE(?, tel_number), 
+                    zip_code = COALESCE(?, zip_code), 
+                    flag = 1, 
+                    icon_image_path = COALESCE(?, icon_image_path)
+                    where id = ?');
+                
                 $sql->execute([
-                    $_POST['name'], $_POST['nickname'], $_POST['address'], $_POST['phonenumber'], $_POST['postcode'],$img_filename, $id
+                    $_POST['name'], $_POST['nickname'], $_POST['address'], $_POST['phonenumber'], $_POST['postcode'], $img_filename, $id
                 ]);
+                
                 $_SESSION['User'] = [
                     'id' => $id, 'name' => $_POST['name'],
                     'nickname' => $_POST['nickname'], 'addres' => $_POST['address'],
@@ -30,10 +44,12 @@
                 header("Location: ./Top.php");
                 exit;
             } else {
-                //　ここは詳細なユーザーデータを登録してなかった人のデータを挿入する処理
-                $sql = $pdo->prepare('insert into User(id, name, nickname, addres, tel_number, zip_code, flag, icon_image_path) values (null,?,?,?,?,?,1,?)');
+                // ここは詳細なユーザーデータを登録してなかった人のデータを挿入する処理
+                $sql = $pdo->prepare('insert into User(id, name, nickname, addres, tel_number, zip_code, flag, icon_image_path) 
+                    values (null,?,?,?,?,?,1,?)');
+                
                 $sql->execute([
-                    $_POST['name'], $_POST['nickname'], $_POST['address'], $_POST['phonenumber'], $_POST['postcode'],$img_path
+                    $_POST['name'], $_POST['nickname'], $_POST['address'], $_POST['phonenumber'], $_POST['postcode'], $img_path
                 ]);
                 header("Location: ./Top.php");
                 exit;
