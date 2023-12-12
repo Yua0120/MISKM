028c8e2faac059c92f277e0be1ddf08d4f5e10ce
m1kqn <137014023+suzuki689@users.noreply.github.com>
2023-12-12 15:36:27 +0900

    ボタン

--- MISKM/Foodies/php/U_look.php
+++ MISKM/Foodies/php/U_look.php
@@ -8,7 +8,6 @@
     <title>master-page</title>
 </header>
 <?php require 'FoodiesTitle.php' ;?>
-<a href="logout-check.php">ログアウト</a>
 <p class="text">ユーザー　一覧</p>
 
 <?php
@@ -46,4 +45,5 @@
         }
     }
 ?>
+<a href="logout-check.php" class="logout-button">ログアウト</a>
 <?php require 'footer.php' ;?>
